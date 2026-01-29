2c80ded9ce4c04b30819932ca92fb7f86fc71162
Jairo Sandoval <jairosandovalg011@gmail.com>
2026-01-28 22:39:56 -0500

    Modelo Dimensional

--- BI/poblar.bi.sql
+++ BI/poblar.bi.sql
@@ -1,56 +1,55 @@
-CREATE TABLE dim_tiempo (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    fecha DATE NOT NULL,
-    anio INT NOT NULL,
-    mes INT NOT NULL,
-    dia INT NOT NULL
-);
+use bd_autmotriz_postventa
+select * from sucursal
 
 
-CREATE TABLE dim_sucursal (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    sucursal_id INT NOT NULL, -- ID del sistema OLTP
-    nombre_sucursal VARCHAR(100) NOT NULL,
-    direccion VARCHAR(200)
-);
+-- Poblar dim_tiempo
+SELECT 
+	DISTINCT CONVERT(DATE,fecha_ingreso) AS 'fecha_transaccion',
+	YEAR(fecha_ingreso) AS 'anio', 
+	MONTH(fecha_ingreso) AS 'mes',
+	DAY(fecha_ingreso) AS 'dia'
+FROM orden_servicio;
 
+-- Poblar dim_servicio
+SELECT
+	id_servicio AS servicio_id,
+	nombre AS 'nombre_servicio',
+	descripcion 
+FROM servicios
 
-CREATE TABLE dim_vehiculo (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    vehiculo_id INT NOT NULL, -- ID del sistema OLTP
-    marca VARCHAR(50),
-    modelo VARCHAR(50),
-    anio INT
-);
+-- poblar dim_sucursal
+SELECT
+	id_sucursal AS 'sucursal_id',
+	nombre AS 'nombre_sucursal',
+	direccion
+FROM sucursal;
 
+-- poblar dim_vehiculo
+select * from vehiculos
+SELECT
+	id_vehiculo AS 'vehiculo_id',
+	marca,
+	modelo,
+	anio
+FROM vehiculos
 
-CREATE TABLE dim_servicio (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    servicio_id INT NOT NULL, -- ID del sistema OLTP
-    nombre_servicio VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(200)
-);
-
-
-CREATE TABLE hechos_servicios (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    dim_tiempo_id INT NOT NULL,
-    dim_sucursal_id INT NOT NULL,
-    dim_vehiculo_id INT NOT NULL,
-    dim_servicio_id INT NOT NULL,
-    dim_tipo_transaccion_id INT NOT NULL,
-    monto DECIMAL(10,2) NOT NULL
-
-    CONSTRAINT fk_hecho_tiempo 
-        FOREIGN KEY (dim_tiempo_id) REFERENCES dim_tiempo(id),
-
-    CONSTRAINT fk_hecho_sucursal 
-        FOREIGN KEY (dim_sucursal_id) REFERENCES dim_sucursal(id),
-
-    CONSTRAINT fk_hecho_vehiculo 
-        FOREIGN KEY (dim_vehiculo_id) REFERENCES dim_vehiculo(id),
-
-    CONSTRAINT fk_hecho_servicio 
-        FOREIGN KEY (dim_servicio_id) REFERENCES dim_servicio(id),
-
-);
+--poblar hechos_transacciones
+SELECT
+	CAST(os.fecha_ingreso as date) dim_tiempo_id,
+    ds.id_sucursal  AS dim_sucursal_id,
+    dv.id_vehiculo  AS dim_vehiculo_id,
+    dse.id_servicio AS dim_servicio_id,
+    os.monto_final AS monto
+FROM orden_servicio os
+INNER JOIN cotizaciones c
+    ON os.id_cotizacion = c.id_cotizacion
+INNER JOIN vehiculos v
+    ON c.id_vehiculo = v.id_vehiculo
+INNER JOIN servicios s
+    ON c.id_servicio = s.id_servicio
+INNER JOIN vehiculos dv
+    ON dv.id_vehiculo = v.id_vehiculo
+INNER JOIN servicios dse
+    ON dse.id_servicio = s.id_servicio
+INNER JOIN sucursal ds
+    ON ds.id_sucursal = v.id_sucursal;
